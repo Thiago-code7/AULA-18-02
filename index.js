af01d55acbfeeb107b668ed13e52a6410f9ccadc
THIAGO DE MOURA MONTEIRO MELO <a95484363@rn.senac.local>
2025-03-10 17:27:36 -0300

    subendo atv 18-02 conexao com banco de dados

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
-
+const {pool} = require('./src/config/database')
 dotenv.config();
 
 const port = process.env.PORTA;
@@ -9,114 +9,126 @@ const app = express();
 //aplicacao use express como json(javascript object notation)
 app.use(express.json());
 
-const bancoDados = [];
 
-app.get('/equipamentos', (requisicao, resposta) => {
+app.get('/equipamentos', async (requisicao, resposta) => {
     //tratamento de excessoes
     try {
-      if (bancoDados.length === 0) {
+      const consulta = 'select * from equipamento'
+      const equipamentos = await pool.query(consulta)
+      if (equipamentos.rows.length === 0) {
         return resposta.status(200).json({ mensagem: "banco de dados vazio" })
       }
-      resposta.status(200).json(bancoDados);
+  
+      resposta.status(200).json(equipamentos.rows);
   
     } catch (error) {
-      resposta.status(500).json({ mensagem: "erro ao buscar Equipamentos", erro: erro.message })
+      resposta.status(500).json({ mensagem: "erro ao buscar equipamentos", erro: error.message })
   
     }
   });
 
-  app.post('/equipamentos', (requisicao, resposta) => {
+  app.post('/equipamentos', async (requisicao, resposta) => {
     try {
-      const { id, nomeFuncionario, nomeEquipamento, dataRetirada, dataDevolucao, status } = requisicao.body;
-      if (!id || !nomeFuncionario || !nomeEquipamento || !dataRetirada || !dataDevolucao || !status) {
+      const {  nomeFuncionario, nomeEquipamento, dataRetirada, dataDevolucao, status } = requisicao.body;
+      if ( !nomeFuncionario || !nomeEquipamento  || !dataRetirada || !dataDevolucao || !status) {
         return resposta.status(200).json(
           {
             mensagem: "Todos os dados devem ser preenchidos corretamente"
           }
         )
       }
-      const novoEquipamento = { id, nomeFuncionario, nomeEquipamento, dataRetirada, dataDevolucao, status  };
-      bancoDados.push(novoEquipamento);
+      const dados = [ nomeFuncionario, nomeEquipamento, dataRetirada, dataDevolucao, status  ];
+      const consulta = `insert into equipamento(nomeFuncionario, nomeEquipamento, dataRetirada, dataDevolucao, status)
+                         values ($1, $2, $3, $4, $5) returning *`
+      const resultado = await pool.query(consulta, dados)
       resposta.status(201).json({ mensagem: "Equipamento criado com sucesso" });
-    } catch (error) { 
+    } catch (error) {
       resposta.status(500).json({
-        mensagem: "Erro ao cadastrar Equipamento",
-        erro: error.message,
-      });
+        mensagem: "erro ao Criar equipamentos",
+        erro: error.message})
     }
   });
 
-  app.put("/equipamento/:id", (requisicao, resposta) => {
+  app.put("/equipamento/:id", async (requisicao, resposta) => {
     try {
       //localhost:3000/produtos/1
       const id = requisicao.params.id;
-      const {  novoFuncionario, novoEquipamento, novaDataRetirada, novaDataDevolucao, novoStatus } = requisicao.body
+      const {  nomeFuncionario, nomeEquipamento, dataRetirada, dataDevolucao, status  } = requisicao.body
       if (!id) {
         return resposta.status(404).json({
           mensagem: "informe um parametro!"
         })
       }
-      const equipamento = bancoDados.find(elemento => elemento.id === id)
-      if (!equipamento) {
-        return resposta.status(404).json({mensagem:"Equipamento nao encontrado"})
+      const dados1 = [id]
+      const consulta1 = `select *from equipamento where id = $1`
+      const resultado1 = await pool.query(consulta1, dados1)
+  
+      if (resultado1.rows.length === 0) {
+        return resposta.status(404).json({mensagem:"Produto nao encontrado"})
       }
-      equipamento.nomeFuncionario = novoFuncionario
-      equipamento.nomeEquipamento = novoEquipamento
-      equipamento.dataRetirada = novaDataRetirada
-      equipamento.dataDevolucao = novaDataDevolucao
-      equipamento.status = novoStatus
-       
+      const dados2 = [id, novoFuncionario, novoEquipamento, novaDataRetirada, novaDataDevolucao, novoStatus]
+      const consulta2 = `update equipamento set nomeFuncionario = $2, nomeEquipamento = $3, dataRetirada = $4, dataDevolucao = $5, status = $5, where id = $1 returning * `
+      await pool.query(consulta2, dados2)
       resposta.status(200).json({ mensagem: "Equipamento atualizado com sucesso" })
     } catch (error) {
       resposta.status(500).json({
-        mensagem: "Erro ao editar Equipamentos!",
-        erro: error.message
-      });
+        mensagem: "erro ao Editar equipamento",
+        erro: error.message})
     }
   })
 
-  app.delete("/equipamentos/:id", (requisicao, resposta) => {
+  app.delete("/equipamentos/:id", async (requisicao, resposta) => {
     try {
-     const id = requisicao.params.id
-     const index = bancoDados.findIndex(elemento => elemento.id === id)
-     if(index === -1){
-       return resposta.status(404).json({mensagem:"equipamento  nao encontrado"})
-     }
-     bancoDados.splice(index, 1)
-     resposta.status(200).json({mensagem:"equipamento deletado com sucesso"})
-    } catch (error) {
-     resposta.status(500).json({
-       mensagem: "erro ao deletar Equipamento",
-       erro: error.message})
+      const id = requisicao.params.id
+      const dados1 = [id]
+      const consulta1 = `select * from equipamento where id = $1`
+      const resultado1 = await pool.query(consulta1, dados1)
+      if(resultado1.rows.length === 0){
+        return resposta.status(404).json({mensagem:"equipamento nao encontrado"})
+      }
+      const dados2 = [id]
+      const consulta2 = `delete from  equipamento where id = $1`
+      await pool.query(consulta2,dados2)
+      resposta.status(200).json({mensagem:"equipamento deletado com sucesso"})
+     } catch (error) {
+      resposta.status(500).json({
+        mensagem: "erro ao deletar equipamento",
+        erro: error.message})
      
     }
    })
 
-   app.get("/equipamentos/:id", (requisicao, resposta) => {
+   app.get("/equipamentos/:id", async (requisicao, resposta) => {
     try {
       const id = requisicao.params.id;
-      const equipamento = bancoDados.find(elemento => elemento.id === id);
-      if(!equipamento){
-        return resposta.status(404).json({mensagem:"equipamento nao encontrado"})
-      }
-      resposta.status(200).json(equipamento)
-    } catch (error) {
-      resposta.status(500).json({
-        mensagem: "erro a buscar equipamento",
-        erro: error.message
-      })
+    const dados1 = [id]
+    const consulta1 = `select * from equipamento where id =$1`
+    const resultado1 = await pool.query(consulta1, dados1)
+    if(resultado1.rows.length === 0){
+      return resposta.status(404).json({mensagem:"equipamento nao encontrado"})
+    }
+    resposta.status(200).json(resultado1.rows[0])
+  } catch (error) {
+    resposta.status(500).json({
+      mensagem: "erro a buscar equipamento",
+      erro: error.message
+    })
       
     }
   })
 
-  app.delete("/equipamentos", (requisicao, resposta) => {
+  app.delete("/equipamentos", async (requisicao, resposta) => {
     try {
-      bancoDados.length = 0;
-      resposta.status(200).json({mensagem:"todos os Equipamentos deletados com sucesso"})
-    } catch (error) {
-      
-    }
-    })
-    app.listen(port, () => {
-      console.log(`Servidor rodando em http://localhost:${port}`);
+      const consulta = `delete from equipamento`
+  await pool.query(consulta)
+  resposta.status(200).json({mensagem:"todos os Equipamentos deletados com sucesso"})
+} catch (error) {
+  resposta.status(500).json({
+    mensagem: "erro ao deletar todos Equipamentos",
+    erro: error.message
+})
+}
+})
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
     });
